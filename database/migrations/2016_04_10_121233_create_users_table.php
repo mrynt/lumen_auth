d325b2e29327cc607d0ddd892d7b029bb80e38d8
marcocastignoli <marco.castignoli@gmail.com>
2016-04-11 01:00:57 +0200

    First commit

---
+++ database/migrations/2016_04_10_121233_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('email');
+            $table->boolean('confirmed');
+            $table->boolean('auth');
+            $table->string('api_token', 60)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
